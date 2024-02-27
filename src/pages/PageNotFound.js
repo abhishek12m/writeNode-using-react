e25399c68998ecbc5f1b87aba096a1f41dbe1f24
Abhishek Maheshwari <61412113+abhishek12m@users.noreply.github.com>
2024-02-27 23:17:26 +0530

    Page Not Found

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,7 +1,12 @@
-
+import { Link } from "react-router-dom"
+import pagenotfound from '../assets/images/pagenotfound.webp';
 
 export const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
+    <section className="pagenotfound">
+        <p> 404 / Page Not Found</p>
+        <img src={pagenotfound} alt="PageNotFound"/>
+        <Link to="/"><button>Back to home</button></Link>
+    </section>
   )
 }
