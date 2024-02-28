ab476105febc6cc6e217e48e3131d9f3fba1725f
Abhishek Maheshwari <61412113+abhishek12m@users.noreply.github.com>
2024-02-28 14:59:04 +0530

    env file

--- src/firbase/config.js
+++ src/firbase/config.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyApjJr5pbmCl34MaUlMdPEULVT66llfB0w",
-    authDomain: "writenode-7645f.firebaseapp.com",
-    projectId: "writenode-7645f",
-    storageBucket: "writenode-7645f.appspot.com",
-    messagingSenderId: "749957205227",
-    appId: "1:749957205227:web:6a8108085c8070117442ae"
+    apiKey: `${process.env.REACT_APP_API_KEY}`,
+    authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+    projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+    storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+    messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+    appId: `${process.env.REACT_APP_API_ID}`
 };
 
 const app = initializeApp(firebaseConfig);
