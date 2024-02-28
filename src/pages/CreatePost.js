8e761dbb6a154c393ff4497b2bd14e6e4b2a03f2
Abhishek Maheshwari <61412113+abhishek12m@users.noreply.github.com>
2024-02-28 12:48:44 +0530

    Create Blog Post

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,16 +1,32 @@
-
-
+import { addDoc, collection } from "firebase/firestore";
+import { db, auth } from "../firbase/config";
+import { useNavigate } from "react-router-dom";
 export const CreatePost = () => {
-  return (
-    <section className="create">
-        <div className="heading">
-            <h1>Add New Post</h1>
-        </div>
-        <form className="createPost">
-            <input type="text" className="title" name="title" placeholder="Title" maxLength="50" required/>
-            <textarea type="text" className="description" name="description" placeholder="Description" maxLength="600" required/>
-            <button type="submit" className="submit">Create</button>
-        </form>
-    </section>
-  )
+    const navigate=useNavigate();
+    const postRef = collection(db, "posts");
+    async function handleCreatePost(event) {
+        event.preventDefault();
+        const document = {
+            title: event.target.title.value,
+            description: event.target.description.value,
+            author:{
+                name:auth.currentUser.displayName,
+                id:auth.currentUser.uid
+            }
+        }
+        await addDoc(postRef, document);
+        navigate("/");
+    }
+    return (
+        <section className="create">
+            <div className="heading">
+                <h1>Add New Post</h1>
+            </div>
+            <form className="createPost" onSubmit={handleCreatePost}>
+                <input type="text" className="title" name="title" placeholder="Title" maxLength="50" required />
+                <textarea type="text" className="description" name="description" placeholder="Description" maxLength="600" required />
+                <button type="submit" className="submit">Create</button>
+            </form>
+        </section>
+    )
 }
